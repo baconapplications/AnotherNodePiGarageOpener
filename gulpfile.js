5eccfb4e67585a15c57071c8d08c23302ea2920a
Randy Bacon <randy@makin.tech>
2017-01-29 16:58:44 -0800

    create gulp file, first pass at publish to make sure will work on pi

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var del = require('del');
+
+/**
+ * clean-js: removes js and js mpas file
+ */
+gulp.task('clean-js', function () {
+  return del([
+    'lib/**/*.js',
+    'lib/**/*.js.map',
+    'server/index.js',
+    'server/index.js.map',
+    'server/lib/**/*.js',
+    'server/lib/**/*.js.map'
+  ]);
+});
+
+gulp.task('clean-dist', function() {
+    return del(
+        'dist/**/*'
+    );
+});
+
+gulp.task('copy', function(){
+   return gulp.src([
+       'package.json',
+       '*lib/**/*.js',
+       '*server/**/*.js',
+       '*client/index.html'
+    ]).pipe(gulp.dest('dist/'));
+});
